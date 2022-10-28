7276455882648ffa5ed09a8d82e00a71464c217a
zweidmitr <zweidmitr@gmail.com>
2022-10-28 15:21:23 +0300

    fix: error test

--- src/main/java/com/zwei/testb/endpoint/ShareController.java
+++ src/main/java/com/zwei/testb/endpoint/ShareController.java
@@ -1,5 +1,6 @@
 package com.zwei.testb.endpoint;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.zwei.testb.dto.DataSetDto;
 import com.zwei.testb.dto.ShareDto;
 import com.zwei.testb.entities.DataSet;
@@ -8,15 +9,20 @@ import com.zwei.testb.services.DataSetService;
 import com.zwei.testb.services.ShareService;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -30,12 +36,14 @@ public class ShareController {
 
     @Autowired
     private ModelMapper modelMapper;
+    private final ObjectMapper objectMapper;
+    private static final Logger LOGGER = LoggerFactory.getLogger(ShareController.class.getSimpleName());
 
     @GetMapping("/")
     public ResponseEntity test() throws IOException {
         try {
             if(6>5) {
-                throw new IllegalArgumentException("Invalid username or password");
+                throw new IllegalArgumentException("mega ERROR");
             }
             return ResponseEntity.ok("it works");
         } catch (Exception e) {
@@ -51,6 +59,17 @@ public class ShareController {
         } catch (IOException e) {
             throw new RuntimeException("error");
         }
+    }
+
+    @ExceptionHandler(value = {NullPointerException.class, IllegalArgumentException.class})
+    public void handleException(Exception e, HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setStatus(HttpStatus.BAD_REQUEST.value());
+        response.setContentType("application/json");
+        response.getWriter().write(objectMapper.writeValueAsString(new HashMap<>() {{
+            put("message", e.getMessage());
+            put("details", "go to deBug");
+        }}));
+        LOGGER.error(e.getLocalizedMessage());
     }
 
 
